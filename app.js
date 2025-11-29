14e8ec139594b5ac4fcd72722b53847319b6e7f3
Zeliana20 <azzeliana20@student.ub.ac.id>
2025-11-29 14:17:42 +0700

    benerin app.listen

--- app.js
+++ app.js
@@ -270,6 +270,6 @@ app.get("/logout", (req, res) => {
 });
 
 // ===================== PORT =====================
-app.listen(3000, () => {
-  console.log("Server berjalan di http://localhost:3000");
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server berjalan di http://0.0.0.0:3000");
 });
